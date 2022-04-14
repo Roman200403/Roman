11d43a2e8a40222d1c65a2b37dcf3b315f982651
319202 <319202@SERVER3>
2022-04-14 11:47:15 +0300

    Сделали метод main

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,32 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-           
+            Firma a = new Firma();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1 - Добавить фирму" + " " +
+                "2 - Вывести вывести фирму" + " " + "3 - Выход");
+                int number = Convert.ToInt32(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        {
+                            a.Input();
+                            break;
+                        }
+                    case 2:
+                        {
+                            a.Print();
+                            break;
+                        }
+                    default:
+                        {
+                            flag = false;
+                            break;
+                        }
+                }
+            }
         }
     }
 }
